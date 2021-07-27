0b939664119b6efbc57ebc31b9cfda2b78d5d8ea
vagman <bmanousakis@gmail.com>
2021-07-27 22:10:20 +0300

    fixed input validation issues (menu)

--- Knowledge Base/main.py
+++ Knowledge Base/main.py
@@ -1,50 +1,57 @@
 """
-
 Prerequisites
 - Python 3.9.X 
 - PIP: python -m pip install -U pip
 - NLTK: pip install nltk
 - run python shell and then run: nltk.download()
+- pip install pandas
 
 Useful links:
 - PIP: https://www.datacamp.com/community/tutorials/python-install-pip?utm_source=adwords_ppc&utm_campaignid=898687156&utm_adgroupid=48947256715&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=229765585186&utm_targetid=aud-299261629574:dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9061576&gclid=Cj0KCQjw9O6HBhCrARIsADx5qCQ_xRiO5QW6FYKz9WLddk8euDFY29ezo-jadSEIbB9PTRpQYeXCIFIaAhbAEALw_wcB
 - NLTK: https://www.nltk.org/index.html
-
+- Pandas: https://pandas.pydata.org/
 """
 
+import nltk
 from  nltk.tokenize import word_tokenize
 import spacy
-from options_menu import OptionsMenu
-
-
+import pandas as pd
+from options_menu import OptionsMenu as om
 
 
 # Read story from txt file
 def read_story(filename: str):
     with open(filename, encoding="utf8") as f:
-        data_source = f.readline()
-    return data_source
+        raw_data = f.readline()
+    return raw_data
 
-def create_knowledge_base(str_data):
+# Creates a list of all the words
+def create_knowledge_base():
     data_source = read_story("Knowledge Base/story.txt")
     return word_tokenize(data_source)
 
-print(create_knowledge_base())
+# tagged = nltk.pos_tag(create_knowledge_base())
+# print(tagged)
 
 while True:
+    menu_choice = om()
+    
     try:
-        menu_choice = OptionsMenu()
-        int(menu_choice.print_options_menu())
+        choice = int(menu_choice.print_options_menu())
 
-        if(menu_choice == 1):
+        if(choice == 1):
             pass
-        elif(menu_choice == 2):
+
+        if(choice == 2):
             pass
-        else:
+
+        if(choice == 3):
             exit(0)
 
+        if(choice < 1 or choice > 3): 
+            print("Oops! Invalid input, please select a choice from 1-3.")
+            continue
 
-        
     except ValueError:
         print("Oops! Invalid input, please select a choice from 1-3.")
         continue
--- Knowledge Base/options_menu.py
+++ Knowledge Base/options_menu.py
@@ -1,6 +1,8 @@
-
 class OptionsMenu():
 
-    def print_options_menu(self):
-        choice = input("\nWelcome to the implementation of a knowledge base, a natural language processing example!\n\n1.  Add a file as a given story to the NLP program.\n2.  Ask a question to the Knowledge Database\n3.  Exit\n\n")
+    def print_options_menu(self) -> int:
+        choice = input("\nWelcome to the implementation of a knowledge base, a natural language processing example!\n\n1.  Add a file as a given story to the NLP program.\n2.  Ask a question to the knowledge database\n3.  Exit\n\n")
         return choice
+        
+            
+        
