61be696bb587dfad2a2f67ba009c00fed6b7a727
vagman <bmanousakis@gmail.com>
2021-08-01 20:48:26 +0300

    implemented main.py

--- Semantic Analyzer/main.py
+++ Semantic Analyzer/main.py
@@ -1,5 +1,25 @@
-from nltk.data import show_cfg
+from nltk import load_parser, load
 import sqlite3
 
-# Read the semantic vocabulary
-show_cfg('semanctics.fcfg')
+# Load our semanctics file
+grammar = load('semantics.fcfg')
+
+# show_cfg('semanctics.fcfg')
+cemantics_parser = load_parser('semantics.fcfg')
+
+query = 'who loves book'
+trees = list(cemantics_parser.parse(query.split()))
+
+answer = trees[0].label()['SEM']
+answer = [s for s in answer if s]
+q = ' '.join(answer)
+print(q)
+
+sqliteConnection = sqlite3.connect('knowledge_db')
+cursor = sqliteConnection.cursor()
+cursor.execute(q + ";")
+records = cursor.fetchall()
+for row in records:
+    print(row[0])
+
+sqliteConnection.close()
